0d71863547fa9db734563d21e0e903d3520015ff
Matthew Gerling <matthewsgerling@gmail.com>
2020-06-01 18:04:47 -0500

    Converting to months

--- Module2/main/camperAgeInput.py
+++ Module2/main/camperAgeInput.py
@@ -1,6 +1,11 @@
-def convert_to_months(age):
-    pass
+def convert_to_months(x):
+    months = int(x) * 12
+    return months
 
 
 if __name__ == '__main__':
     age = input('Give an age for an infant (1-5 years)')
+
+    print(convert_to_months(age))
+
+
--- Module2/test/testFunctions.py
+++ Module2/test/testFunctions.py
@@ -4,7 +4,7 @@ from Module2.main import camperAgeInput
 
 class FunctionTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(False, True)
+        self.assertEqual(camperAgeInput.convert_to_months(3), 36)
 
 
 if __name__ == '__main__':
