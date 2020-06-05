c1cc02db5cf62e7cfdfba3f8094c5cfe46e0a8ac
Matthew Gerling <matthewsgerling@gmail.com>
2020-06-05 09:15:31 -0500

    Finished Average scores

--- InputOutput/main/averageScores.py
+++ InputOutput/main/averageScores.py
@@ -1,2 +1,14 @@
 def average():
-    pass
+    score1 = input('Please enter your first score:')
+    score2 = input('Please enter your second score:')
+    score3 = input('Please enter your third score:')
+    return (int(score1) + int(score2) + int(score3)) / 3
+
+
+if __name__ == '__main__':
+    fName = input("Please Enter your first name:")
+    lName = input("Please Enter your last name:")
+    age = input("Please Enter your age:")
+    avg = average()
+
+    print(f'{lName}, {fName} age: {age} average grade: {avg:0.2f}')
