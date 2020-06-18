909a27e9a0dd8ad2a583f922a93122ffaf01b2e1
Matthew Gerling <matthewsgerling@gmail.com>
2020-06-17 20:02:23 -0500

    Update innerFunctions.py

--- Week6/innerFunctions.py
+++ Week6/innerFunctions.py
@@ -1,8 +1,21 @@
 def measurements(a_list):
-    x = a_list[0]
-    y = a_list[1]
+    if len(a_list) is 2:
+        x = a_list[0]
+        y = a_list[1]
+    else:
+        x = a_list[0]
+        y = x
 
-    return print('Area = ', x * y, 'Perimeter = ', x + x + y + y)
+    print('Area is ', area(x, y))
+    print('perimeter is ', perimeter(x, y))
+
+
+def area(x, y):
+    return x * y
+
+
+def perimeter(x, y):
+    return x + x + y + y
 
 
 if __name__ == '__main__':
