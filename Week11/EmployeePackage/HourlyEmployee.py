36fdd2c4b9edec76b5e6bc75f20f0b54c95c8200
Matthew Gerling <matthewsgerling@gmail.com>
2020-07-04 15:59:28 -0500

    Update HourlyEmployee.py
    Code done lookig at rubric

--- Week11/EmployeePackage/HourlyEmployee.py
+++ Week11/EmployeePackage/HourlyEmployee.py
@@ -2,14 +2,24 @@ from Week11.EmployeePackage.EmployeeClass import Employee
 from _datetime import datetime
 
 
-class SalariedEmployee(Employee):
-    def __init__(self, fname, lname, date, sal):
+class HourlyEmployee(Employee):
+    def __init__(self, fname, lname, date, rate):
         super().__init__(fname, lname)
         self.startDate: datetime = date
-        self.salary: int = sal
+        self.hourlyRate: float = rate
 
-    def give_raise(self, sal):
-        self.salary = sal
+    def give_raise(self, rate):
+        self.hourlyRate = rate
 
     def display(self):
-        print(Employee.display(self), '\nSalary:', self.salary, '\nStart Date:', self.startDate)
+        print(Employee.display(self), '\nHourly Rate:', self.hourlyRate, '\nStart Date:', self.startDate)
+
+
+# Driver
+Employee2 = HourlyEmployee('Matt', 'Gerling', '4, 4, 2020', 10.00)
+Employee2.display()
+
+Employee2.give_raise(14.00)
+Employee2.display()
+
+del Employee2
